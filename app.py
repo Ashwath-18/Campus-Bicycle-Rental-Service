fa0b3725ecb3f56a9b422c3172ac71f496591f07
drastic-mi <mithuntuitionclass@gmail.com>
2026-02-19 23:27:44 +0530

    Grab basic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config import Config
 from database.db_connection import get_db_connection
 
@@ -24,13 +24,64 @@ def test_db():
 def home():
     return render_template('index.html')
 
-@app.route('/grab')
-def grab():
-    return render_template('grab.html')
+
 
 @app.route('/return')
 def return_bike():
     return render_template('return.html')
+
+@app.route('/grab', methods=['GET', 'POST'])
+def grab_bicycle():
+    if request.method == 'POST':
+        roll_no = request.form['roll_no']
+        bicycle_type = request.form['bicycle_type']
+
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        try:
+            cursor.execute("""
+                SELECT * FROM Rentals
+                WHERE roll_no = %s AND return_time IS NULL
+            """, (roll_no,))
+            active_rental = cursor.fetchone()
+
+            if active_rental:
+                return "You already have an active rental."
+
+            cursor.execute("""
+                SELECT * FROM Bicycles
+                WHERE type = %s AND status = 'Available'
+                LIMIT 1
+            """, (bicycle_type,))
+            bicycle = cursor.fetchone()
+
+            if not bicycle:
+                return "No bicycles available."
+
+            cursor.execute("""
+                INSERT INTO Rentals (roll_no, bicycle_id, grab_time)
+                VALUES (%s, %s, NOW())
+            """, (roll_no, bicycle['bicycle_id']))
+
+            cursor.execute("""
+                UPDATE Bicycles
+                SET status = 'In Use'
+                WHERE bicycle_id = %s
+            """, (bicycle['bicycle_id'],))
+
+            conn.commit()
+            return f"Bicycle {bicycle['bicycle_id']} Grabbed Successfully!"
+
+        except Exception as e:
+            conn.rollback()
+            return f"Error: {e}"
+
+        finally:
+            cursor.close()
+            conn.close()
+
+    return render_template('grab.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
